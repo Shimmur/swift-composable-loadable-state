06ddd770dfcef14e2ae29a564b01c7d7dfa95b41
Luke Redpath <lredpath@community.com>
2024-09-26 14:12:27 +0100

    set up example project

---
+++ Examples/LoadableExamples/LoadableExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadableExamplesApp.swift
+//  LoadableExamples
+//
+//  Created by Luke Redpath on 20/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct LoadableExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
